063bcd47d1d5724cef4f475a4a8cf717051cc72d
juanjuan.chen <juanjuan.chen@rea-group.com>
2018-09-10 17:10:49 +0800

    just practice.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts
+  attr_accessor :name, :email
+
+  def initialize (attributes={})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{name} <#{email}"
+  end
 end
